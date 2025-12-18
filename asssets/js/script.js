55811cc1dc05f086496a6b4dd303d6da56b8271e
[lucadm] <[lucadimaio@gmail.com]>
2025-12-18 11:41:03 +0000

    Add empty function in script.js

--- asssets/js/script.js
+++ asssets/js/script.js
@@ -1,1 +1,17 @@
-console.log("connected");
+//create one function for each specific task to avoid using global scope as much as possible
+
+function runGame() {}
+
+function checkAnswer() {}
+
+function calculateCorrectAnswer() {}
+
+function incrementScore() {}
+
+function incrementWrongAnswer() {}
+
+function displayAdditionQuestions() {}
+
+function displaySubtractQuestions() {}
+
+function displayMultiplyQuestions() {}
